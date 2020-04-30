713403125046264182a4545ef50a2ee778d30760
Stewkowski <stevesa23@gmail.com>
2020-04-30 14:37:43 -0500

    Build complete

---
+++ src/test/java/org/launchcode/servicefinder/ServiceFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.launchcode.servicefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
