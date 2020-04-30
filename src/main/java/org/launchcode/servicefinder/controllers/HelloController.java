629f26013f52054de48d70ac387a8e59019329d3
Stewkowski <stevesa23@gmail.com>
2020-04-30 15:28:45 -0500

    Hello World Test

---
+++ src/main/java/org/launchcode/servicefinder/controllers/HelloController.java
@@ -0,0 +1,16 @@
+package org.launchcode.servicefinder.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+    @ResponseBody
+    @GetMapping
+    public String hello(){
+        return "Hello World!";
+    }
+}
+
